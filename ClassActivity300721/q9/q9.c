9f044016552709c69c507fedf7a326d54b2f4386
mooorkey <i.morkey@msn.com>
2021-08-08 14:11:10 +0700

    initial commit

--- ClassActivity300721/q9/q9.c
+++ ClassActivity300721/q9/q9.c
@@ -1,29 +1,3 @@
-/*9) Write a C program that behaves and produces the same output as
-the Python script given below:
-
-########################################
-
-# convert an integer to a hex string
-def to_hex( value ):
-    HEX_DIGITS = '0123456789abcdef'
-    s = ''
-    if (value < 0):
-        value += (1 << 32)  # 32-bit value
-    while True:
-        d = HEX_DIGITS[ value & 0xf ]
-        s = d + s
-        value >>= 4
-        if value == 0:
-            break
-    s = '0x' + s
-    return s
-
-if __name__ == "__main__": # some test code
-    print (to_hex(33))  # 0x21
-    print (to_hex(-1))  # 0xffffffff
-
-########################################*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
